b8b667e1ddcacf7f7c5620b50cd4237aadc96c18
abhishek <abhishek4@gmail.com>
2013-09-17 16:28:57 +0200

    first commit

--- imgWarp/main.cpp
+++ imgWarp/main.cpp
@@ -32,13 +32,13 @@ using namespace std;
 //          amplitude1 = amplitude of second sinusoid
 //          tPeriod1 = time period of the second sinusoid
 //          phi1 = phase of the second sinusoid
-//          direction - direction of warp. 1 for x-direction, 2 for y-direction and 3 for both x and y
+//          
 //
-void warpFunc(Mat input, Mat &out, double amplitude1, double tPeriod1, double phi1, double amplitude2, double tPeriod2, double phi2, int direction)
+void warpFunc(Mat input, Mat &out, double amplitude1, double tPeriod1, double phi1, double amplitude2, double tPeriod2, double phi2)
 {
     int height = input.rows;
     int width = input.cols;
-    
+    int c;
     double xo, yo;
     input.convertTo(input, CV_32FC1); // Convert image to float
     out = Mat(height, width, input.type());
@@ -48,46 +48,22 @@ void warpFunc(Mat input, Mat &out, double amplitude1, double tPeriod1, double ph
     float *indata = (float*)input.data;
     
     
-    //switch direction of warp
-    switch(direction)
-    {
-        case 1:
+   
             for(int j = 0; j < height; j++){
                 for(int i =0; i < width; i++){
-                    xo = amplitude1 * sin((2.0 * PI * (j/tPeriod1)) + phi1) ;
-                    yo = amplitude2 * (sin(2.0 * PI* (i/tPeriod2)) + phi2);
-                    int ix = min(width-1, max(0, (int) (i + xo)));
-                    int iy = min(height-1, max(0, (int) (j)));
-                    outdata[j*width + i] = indata[iy* width + ix];
+                    xo = round(i + amplitude1 * sin(2.0 * PI * (j / tPeriod1) + (phi1*PI/180)));
+                    yo = round(j + amplitude2 * cos(2.0 * PI * (i / tPeriod2) + (phi2*PI/180)));
+                    if(xo >= width || xo < 0 ||
+                       yo >= height || yo < 0)
+                        c = 0;
+                    else
+                        c = indata[int(yo* width + xo)];
+                    outdata[j*width+i] = c;
+                    //                    outdata[int(yo*width + xo)] = indata[j* width + i];
                     
                 }
             }
-            break;
-        case 2:
-            for(int j = 0; j < height; j++){
-                for(int i =0; i < width; i++){
-                    xo = amplitude1 * sin((2.0 * PI * (j/tPeriod1)) + phi1) ;
-                    yo = amplitude2 * (sin(2.0 * PI* (i/tPeriod2)) + phi2);
-                    int ix = min(width-1, max(0, (int) (i)));
-                    int iy = min(height-1, max(0, (int) (j + yo )));
-                    outdata[j*width + i] = indata[iy* width + ix];
-                    
-                }
-            }
-            break;
-        case 3:
-            for(int j = 0; j < height; j++){
-                for(int i =0; i < width; i++){
-                    xo = amplitude1 * sin((2.0 * PI * (j/tPeriod1)) + phi1) ;
-                    yo = amplitude2 * (sin(2.0 * PI* (i/tPeriod2)) + phi2);
-                    int ix = min(width-1, max(0, (int) (i + xo)));
-                    int iy = min(height-1, max(0, (int) (j + yo )));
-                    outdata[j*width + i] = indata[iy* width + ix];
-                    
-                }
-            }
-            break;
-    }
+                
     
     // normalize image
     normalize(out, out,0,255,NORM_MINMAX,CV_8UC1);
@@ -96,14 +72,14 @@ void warpFunc(Mat input, Mat &out, double amplitude1, double tPeriod1, double ph
 
 int main(int argc, const char * argv[])
 {
-
+    
     Mat input = imread("/Users/abhishek/Documents/image\ processing/bauckhage.jpg",CV_LOAD_IMAGE_GRAYSCALE);
     
-    Mat out1, out2, out3;
+    Mat out1,out2;
     
-    warpFunc(input, out1, 30.0, 128.0, 0.0,30.0, 128.0, 0.0, 1);
-    imshow("1", out1);
-
+    warpFunc(input, out1, 0.0, 128.0, 0.0, 15.0, 256.0, 0.0);
+    imshow("res", out1);
+    
     waitKey();
     
     return 0;
